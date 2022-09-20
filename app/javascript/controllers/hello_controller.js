3780c4687ff5a8b40a6eb3804fa4e260b59c3dc0
JamesGoforth <jamesngoforth@gmail.com>
2022-09-20 18:01:34 +0100

    tried to get in carousel and added scroll

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -4,4 +4,11 @@ export default class extends Controller {
   connect() {
     this.element.textContent = "Hello World!"
   }
+
+  function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  };
 }
